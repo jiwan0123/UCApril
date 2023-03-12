6213ba39cc07bf66e37f783519fca90bfcc06f59
snowplum212 <snowplum212@gmail.com>
2023-03-12 20:29:56 +0900

    testtest'

---
+++ Assets/NewBehaviourScript.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NewBehaviourScript : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        Debug.Log("Object moves to the other one"); 
+   
+    }
+}
